3dfa7c7aefbe0330bd259d11a13836a117b55bd5
Nguyen Thanh Thuc <nguyenthanhthuc.2k@gmail.com>
2022-02-21 21:24:15 +0700

    add

--- resources/views/shop/layout/head.blade.php
+++ resources/views/shop/layout/head.blade.php
@@ -14,6 +14,9 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,600,600i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
     @routes()
+    <script>
+        Ziggy.url = 'http://nguyenthanhthuc.cf';
+    </script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- StyleSheet -->
 
